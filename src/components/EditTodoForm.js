b14e77db8b3c5d51685552b61a8b69c8c9f2a1bb
Alexandra Nedelcu <alexandranedelcu@MacBook-Air-Alexandra.local>
2024-07-04 15:58:22 +0300

    Todo List

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+export const EditTodoForm = ({editToDo, task}) => {
+    const [value, setValue] = useState(task.task)
+    const handleSubmit = e => {
+        e.preventDefault();
+        editToDo(value, task.id);
+        setValue('');
+    }
+  return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        className='todo-input' 
+        placeholder='Update Task' 
+        value={value}
+        onChange={(e) => setValue(e.target.value)} />
+        <button type="submit" className='todo-btn'>Update task</button>
+    </form>
+  )
+}
